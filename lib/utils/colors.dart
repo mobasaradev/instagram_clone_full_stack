367b853882c865093a16014d18d8f67209f7ae06
mobasaradev <mobasaradev@gmail.com>
2024-03-07 15:31:14 +0600

    first commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+  static const Color mobileBackground = Color(0xFF000000);
+  static const Color webBackground = Color(0xFF121212);
+  static const Color mobileSearch = Color(0xFF262626);
+  static const Color blue = Color(0xFF0095F6);
+  static const Color primary = Colors.white;
+  static const Color secondary = Colors.grey;
+}
